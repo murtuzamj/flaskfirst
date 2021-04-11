0a98a63d194990a27d4e523bb9cc69316ccd9658
Murtuza <jsdevmh@conais.com>
2021-04-11 13:07:16 +0200

    plain flask

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 import yfinance as yf
 app = Flask(__name__)
 
-@app.route("/stockinfo/<stockname>")
-def hello(stockname):
+@app.route("/")
+def hello():
     return "hello"
     #mystockinfo = yf.Ticker(stockname)
     #return mystockinfo.info
