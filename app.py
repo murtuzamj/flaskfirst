58f1a5170b6aae4c41c8eab3ae84d673189d64eb
Murtuza <jsdevmh@conais.com>
2021-04-11 12:52:10 +0200

    hello enter

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import yfinance as yf
 app = Flask(__name__)
 
 @app.route("/stockinfo/<stockname>")
-def hello():
+def hello(stockname):
     return "hello"
     #mystockinfo = yf.Ticker(stockname)
     #return mystockinfo.info
